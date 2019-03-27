86ca47a454d52726aa921a088a982b5c85969649
ARPoiarkoff <ARPoiarkoff@gmail.com>
2019-03-27 14:12:35 -0400

    new

--- class 2/todo/app.js
+++ class 2/todo/app.js
@@ -2,21 +2,27 @@
 var addButton = document.getElementById("add");
 var taskInput = document.getElementById("task");
 var taskList = document.getElementById("taskList");
+var clearButton = document.getElementById("clear");
 
 // add new item to task list
 addButton.addEventListener("click", function(){
-    var task = taskInput.value;
-    // Don't add an empty string
-    if(task.trim()){
-        // add new task list item
-        var newItem = document.createElement("LI");
-        var taskText = document.createTextNode(task);
-        newItem.appendChild(taskText);
-        // clear text input box
-        taskInput.value = "";
-        taskList.appendChild(newItem);
-    }
-    else{
-        alert("Task cannot be empty");
-    }
+  var task = taskInput.value;
+  // Don't add empty string
+  if(task.trim()){
+    // add new task list item
+    var newItem = document.createElement("LI");
+    var taskText = document.createTextNode(task);
+    newItem.appendChild(taskText);
+    // clear text input box
+    taskInput.value = "";
+    taskList.appendChild(newItem);
+  }
+  else{
+    alert("Task cannot be empty");
+  }
+});
+
+// clear entire list
+clearButton.addEventListener("click", function(){
+  taskList.innerHTML = "";
 });
