7f6eb5997a7ee5b6874f2e2b6153a6c6cf8b82d2
ARPoiarkoff <ARPoiarkoff@gmail.com>
2019-04-03 16:41:34 -0400

    2

--- class 2/TrivaApp/app.js
+++ class 2/TrivaApp/app.js
@@ -1,49 +1,52 @@
-var questionID, question, choiceA, choiceB, choiceC, choiceD, questions, numQuestions, qInfo,
-current = 0;
+
+// initialize variables
+var questionID, question, choiceA, choiceB, choiceC, choiceD, userChoice, questions, numQuestions, qInfo,
+current = 0,
+score = 0,
+points = [];
 
 var defaultQuestions = [
-{
-    question: "Where are the three smallest bones in the human body?",
-    choiceA: "middle ear",
-    choiceB: "nose",
-    choiceC: "toes",
-    choiceD: "eyes",
-    correct: "A"
-},
-{
-    question: "What is the most abundant element in the Universe?",
-    choiceA: "Helium",
-    choiceB: "Oxygen",
-    choiceC: "Lithium",
-    choiceD: "Hydrogen",
-    correct: "D"
-},
-{
-    question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
-    choiceA: "8 days",
-    choiceB: "8 seconds",
-    choiceC: "8 minutes",
-    choiceD: "8 hours",
-    correct: "C"
-},
-{
-    question: "What is 10/2?",
-    choiceA: "5",
-    choiceB: "2",
-    choiceC: "8",
-    choiceD: "9",
-    correct: "A"
-},
-{
-    question: "Which planet has the most moons?",
-    choiceA: "Saturn",
-    choiceB: "Mars",
-    choiceC: "Jupiter",
-    choiceD: "Uranus",
-    correct: "C"
-}];
+    {
+        question: "Where are the three smallest bones in the human body?",
+        choiceA: "middle ear",
+        choiceB: "nose",
+        choiceC: "toes",
+        choiceD: "eyes",
+        correct: "A"
+    },
+    {
+        question: "What is the most abundant element in the Universe?",
+        choiceA: "Helium",
+        choiceB: "Oxygen",
+        choiceC: "Lithium",
+        choiceD: "Hydrogen",
+        correct: "D"
+    },
+    {
+        question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
+        choiceA: "8 days",
+        choiceB: "8 seconds",
+        choiceC: "8 minutes",
+        choiceD: "8 hours",
+        correct: "C"
+    },
+    {
+        question: "What is 10/2?",
+        choiceA: "5",
+        choiceB: "2",
+        choiceC: "8",
+        choiceD: "9",
+        correct: "A"
+    },
+    {
+        question: "Which planet has the most moons?",
+        choiceA: "Saturn",
+        choiceB: "Mars",
+        choiceC: "Jupiter",
+        choiceD: "Uranus",
+        correct: "C"
+    }];
 
-// reference HTML elements
 var elQuiz = document.getElementById("quiz");
 var elQuizStatus = document.getElementById("quizStatus");
 
@@ -57,6 +60,7 @@ var elChoices = document.getElementsByName('choices');
 // start quiz
 populateQuestions();
 renderQuestion();
+document.getElementById("submit").onclick = gradeQuestion;
 
 function populateQuestions(){
     // populate with default questions
@@ -72,7 +76,6 @@ function populateQuestions(){
 }
 
 function populateQuestionInfo(){
-    // populate current question info from question list
     question = questions[current].question;
     qInfo = questions[current];
     choiceA = qInfo.choiceA;
@@ -83,7 +86,6 @@ function populateQuestionInfo(){
 }
 
 function renderQuestion(){
-    // display question on webpage
     questionID = current + 1;
     elQuizStatus.innerHTML = "Question " + (questionID) + " of " + (numQuestions);
     populateQuestionInfo();
@@ -92,4 +94,61 @@ function renderQuestion(){
     elChoiceB.innerHTML = choiceB;
     elChoiceC.innerHTML = choiceC;
     elChoiceD.innerHTML = choiceD;
+}
+
+function gradeQuestion(){
+    if(getUserChoice()){
+        if(userChoice == questions[current].correct){
+            score++;
+            points[current] = 1;
+        }
+        else{
+            points[current] = 0;
+        }
+
+        if(current == questions.length-1){
+            endGame();
+        }
+        else{
+            // next question
+            current++;
+            renderQuestion();
+        }
+    }
+}
+
+function getUserChoice(){
+    for (var i = 0, length = elChoices.length; i < length; i++)
+    {
+        if (elChoices[i].checked)
+        {
+            userChoice = elChoices[i].value;
+
+            // clear radio input for next question
+            elChoices[i].checked = false;
+
+            return true;
+        }
+    }
+    // user didn't select an answer
+    alert("Please select an answer before continuing");
+    return false;
+}
+
+function endGame(){
+    elQuiz.innerHTML = "<h2>Your Score: " + score + " out of " + numQuestions + "</h2>";
+    for(var i = 0; i < points.length; i++){
+        var summary = document.createElement("p");
+        if(points[i] == 0){
+            summary.innerHTML = "Question #" + (i+1) + ": INCORRECT";
+            summary.style.color = "red";
+        }
+        else{
+            summary.innerHTML = "Question #" + (i+1) + ": CORRECT!";
+            summary.style.color = "green";
+        }
+        elQuiz.appendChild(summary); 
+    }
+    document.getElementById("options").style.display = "block";
+
 }
